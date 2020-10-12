f13fde269d299880dc10997ceef8d3e41b620ec7
Ian Harold <ianhunterharold@gmail.com>
2020-10-12 17:35:23 -0600

    input form for adding a new color:

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { View, StyleSheet, FlatList, Alert } from "react-native";
+import ColorButton from "./components/ColorButton";
+import defaultColors from './data/defaultColors.json';
+import ColorForm from './components/ColorForm';
+
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("black");
+
+  return (
+    <>
+    <ColorForm 
+      onNewColor={newColor=> Alert.alert(`Add color: ${newColor}`)}
+    />
+    <FlatList style={[styles.container, { backgroundColor }]} 
+    data = {defaultColors}
+    renderItem={ ({item}) => {
+      return (
+        <ColorButton key={item.id} backgroundColor={item.color} onPress={setBackgroundColor} />
+      )
+    }}
+    />
+    </>  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+  }
+});
---
+++ components/ColorForm.js
@@ -0,0 +1,52 @@
+import React, {useState, useRef} from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+
+function ColorForm( {onNewColor = f => f} ){
+    const [inputValue, setValue] = useState("");
+    const input = useRef();
+    return(
+        <View style={styles.container}>
+            <TextInput 
+            ref={input}
+            style={styles.txtInput} 
+            autoCapitalize="none"
+            placeholder="enter a color"
+            value={inputValue}
+            onChangeText={setValue}
+            >
+            </TextInput>
+            <Button 
+            title="Add Color"
+            onPress= { ()=> {
+                input.current.blur();
+                onNewColor(inputValue);
+                setValue("");
+            }}
+            
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        flexDirection: 'row',
+        alignItems: 'center'
+
+    },
+    txtInput: {
+        flex: 1,
+        borderWidth: 2,
+        fontSize: 20,
+        margin: 5,
+        borderRadius: 5,
+        padding: 5
+    }
+
+})
+
+
+
+export default ColorForm;
